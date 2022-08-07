b5c7795eb33a960679a6d9aa155f780427be8a50
김태홍 <thkim@brandazine.com>
2022-08-08 03:02:56 +0900

    Add database tables in model

--- querytest/models.py
+++ querytest/models.py
@@ -1,3 +1,35 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+"""
+product <- product attr <- stock  <-
+stockbox                          <- stockboxitem
+"""
+
+class Product(models.Model):
+    name: str = models.CharField(null=False)
+    price: int = models.PositiveIntegerField(null=False)
+
+    
+class ProductAttr(models.Model):
+    product: Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sizes: str = models.CharField(null=False)
+    style: str = models.CharField(null=False)
+
+    
+class Stock(models.Model):
+    product_attr: ProductAttr = models.ForeignKey(ProductAttr, on_delete=models.CASCADE)
+    code: str = models.CharField(null=False)
+
+    
+class StockBox(models.Model):
+    address: str = models.CharField(null=False)
+    created_time: datetime = models.DateTimeField(auto_created=True)
+    
+
+class StockBoxItem(models.Model):
+    stock: Stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock_box: StockBox = models.ForeignKey(StockBox, on_delete=models.CASCADE)
+    
+    
+## 추후 many to many, one to one 도 포함하기
